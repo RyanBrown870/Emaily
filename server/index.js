779182bbe8622ca6ebc6fae7a2ce41d0ab86e8da
rxb870 <ryanbrown0689@gmail.com>
2020-11-16 21:57:06 +0000

    Google OAuth Initialised

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bro: 'fist' }) 
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback", // This can be whatever you want (doesn't need to be auth etc)
+    },
+    (accessToken) => {
+      console.log(accessToken); // Callback function as second argument
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
