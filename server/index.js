a186d68e512f97a2a08fc27ae9230a06e740e825
rxb870 <ryanbrown0689@gmail.com>
2020-11-17 13:58:12 +0000

    passport authentication done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const keys = require("./config/keys");
 
 const app = express();
 
+// Set up passport with client id etc and callback url we will be sent back to.
 passport.use(
   new GoogleStrategy(
     {
@@ -12,18 +13,26 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback", // This can be whatever you want (doesn't need to be auth etc)
     },
-    (accessToken) => {
-      console.log(accessToken); // Callback function as second argument
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken); // Callback function as second argument. This fires after second route handler get's access token from google
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
     }
   )
 );
 
+// First route handler to get the code from google for that user
 app.get(
-  "/auth/google",
+  "/auth/google",                         // Callback address that google sends us back to after oauth
   passport.authenticate("google", {
     scope: ["profile", "email"],
   })
 );
+
+// Second route handler to exchange code from url (after auth/google/callback) to get user info
+app.get(
+  '/auth/google/callback', passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
