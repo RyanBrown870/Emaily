96f1755a32cb9acf870c126142a92d29ef68f032
rxb870 <ryanbrown0689@gmail.com>
2020-11-21 12:57:47 +0000

    finished auth flow

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+// get the keys from the environment variable. Capitalisation is convention
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
