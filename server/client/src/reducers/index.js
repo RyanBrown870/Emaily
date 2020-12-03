f9d16bb41667c5c711f0169d445d60d4cd8371af
rxb870 <18215428+rxb870@users.noreply.github.com>
2020-12-03 21:41:08 +0000

    Survey new and survey form initialised

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 
 // keys in the below object are the keys which represent state.
 export default combineReducers ({
-    auth: authReducer
+    auth: authReducer,
+    form: reduxForm
 });
