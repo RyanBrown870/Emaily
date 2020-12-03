5a9b7412b68ed605271caac0a306aa03cd3954e0
rxb870 <18215428+rxb870@users.noreply.github.com>
2020-12-03 18:44:18 +0000

    Email surveys backend working

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -7,7 +7,10 @@ import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
 import reducers from './reducers';
 import reduxThunk from "redux-thunk"; 
- 
+
+// Development only axios helpers
+import axios from 'axios' ;
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -9,7 +9,12 @@ const Survey = mongoose.model('surveys');
 // Can keep including arguments to post() for as long as you like before calling the req/res function.
 // Need to check if logged in and also if they have enough credits.
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    })
+
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -23,7 +28,17 @@ module.exports = app => {
 
         // Great place to send an email
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
 
+        try {
+        await mailer.send();
+        await survey.save();                    // save the survey
+        req.user.credits -= 1;                  // subtract a credit from the user
+        const user = await req.user.save();     // save the user record
+
+        res.send(user);
+        } catch (err) {
+            res.status(422).send(err);                    // unprocessable entry (some user error)
+        }                                                 // send the error
+        
     });
 };
